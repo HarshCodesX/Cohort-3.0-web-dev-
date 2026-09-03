b3834c5e979b58454f81e3c3dfe6c9f5ad184d35
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-03 19:38:02 +0530

    abstract classes in typescript

--- week-14/14.2/src/two.ts
+++ week-14/14.2/src/two.ts
@@ -24,3 +24,27 @@ let user = new Manager("John", 30);
 console.log(user.age);
 let legal = user.isLegal(55)
 console.log(legal);
+
+// abstract class example
+
+abstract class Human{
+    name: string;
+    constructor(name: string){
+        this.name = name;
+    }
+    abstract greet(): string;
+    hello(){
+        console.log("hey there");
+    }
+}
+
+class Employee extends Human{
+    name: string;
+    constructor(name: string){
+        super(name)
+        this.name = name;
+    }
+    greet(){
+        return "hi " + this.name
+    }
+}
