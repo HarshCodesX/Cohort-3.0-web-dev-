bcf59d5674394d3fda28e5f86c4286431c947652
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-04 16:20:16 +0530

    added example for union and intersection of types in typescript

--- week-14/14.2/src/third.ts
+++ week-14/14.2/src/third.ts
@@ -2,11 +2,42 @@
 // diff between types and interfaces is that classes can also implement the interface
 // types let you do unions and intersections (this is another diff between types and interfaces)
 
-type User = {
+
+// Intersection example (it can have all the properties of each type)
+type Employee = {
     name: string;
-    age: number
+    startDate: Date;
 }
 
-function isLegal(user: User){
-    return user.age > 18;
+type Manager = {
+    name: string;
+    department: string;
+}
+
+type TeamLead = Employee & Manager
+
+const teamLead: TeamLead = {
+    name: "harkirat",
+    startDate: new Date(),
+    department: "Software developer"
+};
+
+// Union example (it can have either properties of each type as well as all the properties of each type)
+
+type GoodUser = {
+    name: string;
+    gift: string;
+}
+
+type BadUser = {
+    name: string;
+    ip: string;
+}
+
+type User = GoodUser | BadUser;
+
+const user: User = {
+    name: "harsh",
+    gift: "ps5",
+    ip: "dfg8"
 }
