62740ed254c837cf9970c3e36550a52d29b204cc
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-07 17:13:16 +0530

    maps in typescript

--- week-14/14.3/src/six.ts
+++ week-14/14.3/src/six.ts
@@ -1,6 +1,12 @@
 // Maps in typescript
 
-const users = new Map();
+type User = {
+    name: string;
+    age: number;
+    email: string;
+}
+
+const users = new Map<string, User>();
 users.set("ras@qd1", {name: "Ras", age: 30, email: "ras@qd1"});
 users.set("sarah@qd1", {name: "Sarah", age: 28, email: "sarah@qd1"});
 
