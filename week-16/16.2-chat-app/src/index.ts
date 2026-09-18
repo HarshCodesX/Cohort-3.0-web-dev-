8d8ff267009b09db44b20060f2541ce986d6d023
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-19 02:30:42 +0530

    added be logic for broadcasting

--- week-16/16.2-chat-app/src/index.ts
+++ week-16/16.2-chat-app/src/index.ts
@@ -1,18 +1,23 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({port: 8080});
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
 wss.on("connection", (socket) => {
+    allSockets.push(socket);
     userCount = userCount + 1; 
     console.log("user connected # " + userCount);
 
     socket.on("message", (message)=>{
-        console.log(message.toString());
         setTimeout(() => {
-            socket.send(message.toString() + ": sent from the server!");
+            // console.log("message received from client: " + message.toString());
+            allSockets.forEach((socket) => socket.send(message.toString() + " : sent from the server"));
         }, 1500);
-        
+    });
+
+    socket.on("disconnect", () => {
+        allSockets = allSockets.filter((s) => s !== socket);
     });
 })
