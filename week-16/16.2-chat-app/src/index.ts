b27e0366750d1329a6a799d166e296f3c61dcd2a
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-18 19:52:14 +0530

    added code for connection and message logging!

---
+++ week-16/16.2-chat-app/src/index.ts
@@ -0,0 +1,18 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8080});
+
+let userCount = 0;
+
+wss.on("connection", (socket) => {
+    userCount = userCount + 1; 
+    console.log("user connected # " + userCount);
+
+    socket.on("message", (message)=>{
+        console.log(message.toString());
+        setTimeout(() => {
+            socket.send(message.toString() + ": sent from the server!");
+        }, 1500);
+        
+    });
+})
