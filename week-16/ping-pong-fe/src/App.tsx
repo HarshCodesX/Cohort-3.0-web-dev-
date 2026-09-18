52e1d5d0a0347f4d86b8755a138a075fea08e455
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-18 16:52:10 +0530

    added boiler code in the FE

--- week-16/ping-pong-fe/src/App.tsx
+++ week-16/ping-pong-fe/src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const App = () => {
 
   function sendMessage() {
 
   }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    ws.onerror = () => {
+      console.log("Cannot connect to the websocket");
+    }
+    ws.close = () => {
+      console.log("Connection to the websocket closed");
+    }
+    ws.onopen = () => {
+      console.log("Connected to the websocket");
+    }
+  }, []);
 
   return (
     <div>
