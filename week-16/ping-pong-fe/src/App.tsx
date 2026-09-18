406f4da362f0a46a407122aa662d0a5bac69d7e9
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-18 17:13:59 +0530

    added fe code

--- week-16/ping-pong-fe/src/App.tsx
+++ week-16/ping-pong-fe/src/App.tsx
@@ -1,13 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const App = () => {
+  const [socket, setSocket] = useState();
+  const inuputRef = useRef();
 
   function sendMessage() {
-
+    if(!socket){
+      return;
+    }
+    const message = inuputRef.current.value;
+    //@ts-expect-error
+    socket.send(message);
   }
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
     ws.onerror = () => {
       console.log("Cannot connect to the websocket");
     }
@@ -17,11 +25,14 @@ const App = () => {
     ws.onopen = () => {
       console.log("Connected to the websocket");
     }
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    }
   }, []);
 
   return (
     <div>
-      <input type="text" placeholder='message' />
+      <input ref={inuputRef} type="text" placeholder='message' />
       <button onClick={sendMessage}>Send</button>
     </div>
   )
