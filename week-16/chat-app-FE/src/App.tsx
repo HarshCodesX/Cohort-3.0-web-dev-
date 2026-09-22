78e6786aa42185cd219486148f3d38e9c5866b79
gupta-harsh1 <harshguptahb055@gmail.com>
2026-09-22 22:34:40 +0530

    added fe code

--- week-16/chat-app-FE/src/App.tsx
+++ week-16/chat-app-FE/src/App.tsx
@@ -1,17 +1,47 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const App = () => {
+  const [messages, setMessages] = useState(["hi there", "hello"]);
+  const wsRef = useRef();
 
   useEffect(() => {
     const ws = new WebSocket("http://localhost:8080");
+    wsRef.current = ws;
+    ws.onmessage = (event) => {
+      setMessages((m) => [...m, event.data]);
+    }
+    // hardcoding the joining room logic for now, will change later
+    ws.onopen = (event) => {
+      ws.send({
+        ws.send(JSON.stringify({
+          type: "join",
+          payload: {
+            roomId: "green"
+          }
+        }))
+      })
+    }
   }, []);
 
   return (
     <div className="h-screen bg-black">
-      <div className='h-[95vh]'></div>
+      <br />
+      <div className='h-[85vh]'>
+        {messages.map((message) => <div className='m-8'>
+          <span className='text-black bg-white rounded p-4'>{message}</span>
+          </div>)}
+      </div>
       <div className='w-full bg-white flex'>
-        <input type="text" className='flex-1 p-4' />
-        <button className='bg-purple-600 text-white p-4'>Send message</button>
+        <input id='message' type="text" className='flex-1 p-4' />
+        <button onClick={() => {
+          const message = document.getElementById('message')?.value;
+          wsRef.current.send(JSON.stringify({
+            type: "chat",
+            payload: {
+              message: message
+            }
+          }));
+        }} className='bg-purple-600 text-white p-4'>Send message</button>
       </div>
     </div>
   )
